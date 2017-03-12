dffd5a419ce82bf9b9d0bb141c86090eb722cb5d
swan <gordienko.c@gmail.com>
2017-03-12 14:53:30 +0200

    Added bootstrap to template. Added grid classes. Created progress bars and buttons. Moved special block to separate template file.

--- code/src/Fallout/GameBundle/Controller/DefaultController.php
+++ code/src/Fallout/GameBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GameBundle::main.screen.html.twig');
+        return $this->render('GameBundle::main.screen.html.twig', [
+            'strength_value' => 50,
+            'agility_value' => 50,
+            'perceive_value' => 50,
+            'luck_value' => 50,
+        ]);
     }
 }
