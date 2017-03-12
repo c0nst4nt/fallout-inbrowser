99385bfdf7d8f5b924edbea27bbbe49168f53f0c
swan <gordienko.c@gmail.com>
2017-03-12 14:24:26 +0200

    Added node js container. Added package json for npm modules. Wrote basic layout for main page. Changed image, deleted one character picture, mirrored it. Wrote simple basic css style file.

--- code/src/Fallout/GameBundle/Controller/DefaultController.php
+++ code/src/Fallout/GameBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GameBundle:Default:index.html.twig');
+        return $this->render('GameBundle::main.screen.html.twig');
     }
 }
