395a15420d4bb5833c205d8b9397209dfa7cb24f
swan <gordienko.c@gmail.com>
2017-03-20 01:05:09 +0200

    Added checking of code folder, waiting till install

--- environment/node/docker-entrypoint.sh
+++ environment/node/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+while [ ! -d /var/www/src/Fallout/GameBundle/Resources/frontend ]; do
+    echo "Waiting for package.json..."
+    sleep 2s;
+done
+
+cd /var/www/src/Fallout/GameBundle/Resources/frontend
 npm install;
 ./node_modules/.bin/webpack;
 
